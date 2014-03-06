013416cfed65846cb0a08097bc0cc7bd27eafc57
Marek Żarnowski <marek1840@gmail.com>
2014-03-06 13:26:40 +0100

    moved Number class to Expression file

--- src/main/scala/pl/edu/agh/mplt/parser/expression/Expression.scala
+++ src/main/scala/pl/edu/agh/mplt/parser/expression/Expression.scala
@@ -1,8 +1,12 @@
 package pl.edu.agh.mplt.parser.expression
 
-trait Expression
+import pl.edu.agh.mplt.parser.IntercodeExpression
+
+trait Expression extends IntercodeExpression
 
 case class Parenthesized(expr: Expression) extends Expression
 
+case class Number(v: String) extends Expression
 
+case class StringLiteral(str: String) extends Expression
 
--- src/main/scala/pl/edu/agh/mplt/parser/expression/arithmetic/Number.scala
+++
@@ -1,5 +0,0 @@
-package pl.edu.agh.mplt.parser.expression.arithmetic
-
-import pl.edu.agh.mplt.parser.expression.Expression
-
-case class Number(v: String) extends Expression

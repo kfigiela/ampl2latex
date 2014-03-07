3ee3f79361407b11f9986fd14e1a397aa8a5229d
Marek Żarnowski <marek1840@gmail.com>
2014-03-07 13:00:39 +0100

    added Variable class

--- src/main/scala/pl/edu/agh/mplt/parser/expression/ExpressionAMPLParser.scala
+++ src/main/scala/pl/edu/agh/mplt/parser/expression/ExpressionAMPLParser.scala
@@ -1,17 +1,20 @@
 package pl.edu.agh.mplt.parser.expression
 
 import scala.util.parsing.combinator.JavaTokenParsers
+import pl.edu.agh.mplt.parser.expression.variable.Variable
 
 trait ExpressionAMPLParser extends JavaTokenParsers {
   def arithmeticExpression: Parser[Expression]
 
   def number = floatingPointNumber ^^ Number
 
+  def variable: Parser[Variable] = "\\w+".r ^^ Variable
+
   def string: Parser[StringLiteral] = stringLiteral ^^ { case a => StringLiteral(a.drop(1).dropRight(1))}
 
   def expr: Parser[Expression] = arithmeticExpression | nonRecursiveProductionsParser
 
-  def nonRecursiveProductionsParser: Parser[Expression] = List(number, string) reduce (_ | _)
+  def nonRecursiveProductionsParser: Parser[Expression] = List(number, variable, string) reduce (_ | _)
 
 }
 
--- src/main/scala/pl/edu/agh/mplt/parser/expression/variable/Variable.scala
+++ src/main/scala/pl/edu/agh/mplt/parser/expression/variable/Variable.scala
@@ -2,4 +2,4 @@ package pl.edu.agh.mplt.parser.expression.variable
 
 import pl.edu.agh.mplt.parser.expression.Expression
 
-case class Variable extends Expression
+case class Variable(id: String) extends Expression

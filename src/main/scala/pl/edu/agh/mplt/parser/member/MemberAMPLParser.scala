48f11750f058a1fa6307bd1e6a2381489632bd0f
Marek Żarnowski <marek1840@gmail.com>
2014-03-27 15:43:22 +0100

    member parser now parses also multidimensional members

--- src/main/scala/pl/edu/agh/mplt/parser/member/MemberAMPLParser.scala
+++ src/main/scala/pl/edu/agh/mplt/parser/member/MemberAMPLParser.scala
@@ -2,12 +2,15 @@ package pl.edu.agh.mplt.parser.member
 
 import scala.util.parsing.combinator.JavaTokenParsers
 import pl.edu.agh.mplt.parser.formula.expression.Expression
+import pl.edu.agh.mplt.parser.member.Member
 
 
 trait MemberAMPLParser extends JavaTokenParsers {
   def expr: Parser[Expression]
 
-  def stringLit: Parser[StringMember] = stringLiteral ^^ { case a => StringMember(a.drop(1).dropRight(1))}
+  private def stringLit: Parser[StringMember] = stringLiteral ^^ { case a => StringMember(a.drop(1).dropRight(1))}
 
-  def member: Parser[Member] = stringLit | expr ^^ ExpressionMember
+  private def singleMember: Parser[Member] = stringLit | expr ^^ ExpressionMember
+
+  def member: Parser[Member] = singleMember | "(" ~> rep1sep(singleMember, ",") ^^ MultiMember
 }

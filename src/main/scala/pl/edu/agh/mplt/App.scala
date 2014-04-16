0abf01549194edb13f453e9c049eeb288b8ca06f
Marek Żarnowski <marek1840@gmail.com>
2014-04-16 20:06:01 +0200

    added output to file for parser

--- src/main/scala/pl/edu/agh/mplt/App.scala
+++ src/main/scala/pl/edu/agh/mplt/App.scala
@@ -1,19 +1,22 @@
 package pl.edu.agh.mplt
 
-import java.io.File
-
+import java.io.{PrintWriter, File}
 
 object App {
   def main(args: Array[String]) {
-    if (args.size < 1) {
-      println("Please specify AMPL file to parse")
+    if (args.size < 2) {
+      println("Please specify input and output files")
     } else {
       val start = System.currentTimeMillis()
       val parsedFile = ParsedFile.fromAMPL(new File(args(0)))
       println("\n\n in: " + (System.currentTimeMillis() - start))
 
-      //      println(parsedFile.ast)
+      val out = new PrintWriter(new File(args(1)))
+      try {
+        parsedFile.ast.foreach(line => out.println(line.toString))
+        println(parsedFile.ast)
 
+      } finally {out.close() }
     }
   }
 }

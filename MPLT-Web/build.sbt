30fa0b798c660ac2881059bc211506e3eea65a51
Marek Żarnowski <marek1840@gmail.com>
2014-10-27 10:05:00 +0100

    minor fix

--- MPLT-Web/build.sbt
+++
@@ -1,7 +0,0 @@
-name := "MPLT-Web"
-
-version := "1.0"
-
-scalaVersion := "2.10.2"
-
-libraryDependencies += "org.scalatra" %% "scalatra" % "2.2.2"
--- MPLT-Web/project/WebBuild.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-object WebBuild extends Build{
-  lazy val root = Project(id = "MPLT-Web", base = file(".")).dependsOn(mplt)
-
-  lazy val mplt = RootProject(file("../"))
-}

bfb5b3ece83ad83bdfe315232c6a85cc19337c3e
Kamil Figiela <kamil.figiela@gmail.com>
2015-04-03 18:03:02 +0200

    Fix dequotedLiteral and move it to KeywordAMPLParser

--- src/main/scala/pl/edu/agh/mplt/parser/AMPLParser.scala
+++ src/main/scala/pl/edu/agh/mplt/parser/AMPLParser.scala
@@ -21,8 +21,6 @@ class AMPLParser extends JavaTokenParsers with AssertionAMPLParser with KeywordA
                          with SetExpressionAMPLParser with IndexingAMPLParser with MemberAMPLParser
                          with ReferenceParser {
 
-   def dequotedLiteral: Parser[String] = stringLiteral ^^ {str => str.substring(1, str.length - 1)}
-
    def parse(input: String): ParseResult[Declaration] = parseAll(declaration, input)
 
    private def declaration: Parser[Declaration] =
